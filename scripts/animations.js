dc73469e560cf31742b344df09c031ae295979ce
mackay2588 <mackay2588@gmail.com>
2018-04-19 00:43:52 -0500

    add hero logo animation
    parallax effect and keyframe glow animations

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,3 +1,44 @@
+$(document).ready(function() {
+
+	var request = null;
+	var mouse = { x: 0, y: 0 };
+	var cx = window.innerWidth / 3;
+	var cy = window.innerHeight / 3;
+
+	$("#hero").mousemove(function(event) {
+
+		mouse.x = event.pageX;
+		mouse.y = event.pageY;
+
+		cancelAnimationFrame(request);
+		request = requestAnimationFrame(update);
+	});
+
+	function update() {
+
+		dx = mouse.x - cx;
+		dy = mouse.y - cy;
+
+		tiltx = (dy / cy) ;
+		tilty = - (dx / cx);
+
+
+		radius = Math.sqrt(Math.pow(tiltx,2) + Math.pow(tilty,2));
+		degree = (radius * 10);
+
+		TweenLite.to(".heroLogo", 0.1, {transform:'rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)'});
+
+  }
+
+	$(window).resize(function() {
+		cx = window.innerWidth / 3;
+		cy = window.innerHeight / 3;
+	});
+});
+
+
+
+
 
 /*animations on scroll*/
 
